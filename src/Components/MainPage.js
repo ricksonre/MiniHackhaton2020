7154c413d68def43eef3b294a3c21c4a814eecbf
Carter Phillips <carter.joel@gmx.com>
2020-10-31 10:44:53 -0700

    added conditional page rendering

--- src/Components/MainPage.js
+++ src/Components/MainPage.js
@@ -3,7 +3,6 @@ import {withStyles} from "@material-ui/core/styles";
 import logo from "../logo.svg";
 import firebase from 'firebase';
 import background from "../back.png";
-import MainPage from './MainPage';
 
 
 const firebaseConfig = {
@@ -29,7 +28,7 @@ const styles = (theme) =>
 		}
 	})
 
-class MainComponent extends Component
+class MainPage extends Component
 {
 	constructor(props) {
 		super(props);
@@ -37,18 +36,25 @@ class MainComponent extends Component
 		//it is settable with this.setState({attribute: value})
 		//will only override attribute and no other attributes
 		this.state={
-			openPage: 'MainPage',
+			names: 'Carter, Ren, Aedan, Rick',
+			showHidden: false,
 		}
 	}
 
-	renderPage = (param) => {
-		switch(param)
-		{
-			case 'MainPage':
-				return (<MainPage/>)
-			default:
-				return (<MainPage/>)
-		}
+	example = () =>
+	{
+		//example function
+		//use arrow functions so we can include it in our components
+		//when you include it don't add the () to the end of the name, just do example
+		//unless you are calling it within this file
+
+		//if you need to include arguments, do something like
+		// example = (arg1, arg2 ... ) => {function stuff}
+
+	}
+
+	toggleShow = () => {
+		this.setState({showHidden: ! this.state.showHidden})
 	}
 
 	render()
@@ -61,7 +67,24 @@ class MainComponent extends Component
 		return(
 			<div className="App" style={{backgroundImage: background}}>
 				<header className="App-header" >
-					{this.renderPage(this.state.openPage)}
+					<div style={{position: 'absolute', top: 0}}>
+						<div onClick={this.toggleShow}>
+							<h1>Trick or Tweet</h1>
+						</div>
+						<div style={{marginTop: '10em'}} onClick={this.toggleShow}>
+							<h2>Go to a random house</h2>
+						</div>
+						<div onClick={this.toggleShow}>
+							<h2>Post your house</h2>
+						</div>
+						<div onClick={this.toggleShow}>
+							<h3 style={{marginTop: '5em'}}>Setup Account</h3>
+						</div>
+						{this.state.showHidden ?
+							(<h1>{this.state.names}</h1>):
+							(null)
+						}
+					</div>
 				</header>
 			</div>
 		)
@@ -70,4 +93,4 @@ class MainComponent extends Component
 
 }
 
-export default withStyles(styles)(MainComponent);
+export default withStyles(styles)(MainPage);
