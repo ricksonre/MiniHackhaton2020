ea12c05ba11deb64e246ee36fa4e7000ed5a79fe
Carter Phillips <carter.joel@gmx.com>
2020-10-31 10:07:17 -0700

    Initial project commit

---
+++ src/App.tsx
@@ -0,0 +1,12 @@
+import React from 'react';
+import './App.css';
+import mainComponent from './Components/MainComponent';
+import MainComponent from "./Components/MainComponent";
+
+function App() {
+  return (
+        <MainComponent/>
+  );
+}
+
+export default App;
